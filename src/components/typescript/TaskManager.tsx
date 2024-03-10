f65e13fb0192771e8cdcdc653eeed6d243d84e11
Hevitriniavo <hei.tantely@gmail.com>
2024-03-10 07:46:58 +0300

    feat: using hooks useTaskManager in TaskManager

--- src/components/typescript/TaskManager.tsx
+++ src/components/typescript/TaskManager.tsx
@@ -1,49 +1,8 @@
-import { nanoid } from "nanoid";
-import {ChangeEvent, useState} from "react";
 import "../../styles/TaskManager.css";
-import {Task, TaskUpdate} from "../../types/type.ts";
+import {useTaskManager} from "../../hooks/useTaskManager.tsx";
 
 export const TaskManager = () => {
-  const [title, setTitle] = useState<string>("");
-  const [searchKeyword, setSearchKeyword] = useState<string>("");
-  const [tasks, setTasks] = useState<Task[]>([]);
-
-  // remove task from list
-  const completeTask = (id: string) => {
-    setTasks(tasks.filter((task) => task.id !== id));
-  };
-
-  const updateTask = (id: string, taskUpdate: TaskUpdate) => {
-
-    const index = tasks.findIndex((task) => task.id === id);
-    if (index != -1) {
-      const newTasks = tasks.slice();
-      newTasks[index] = {...newTasks[index], ...taskUpdate };
-      setTasks(newTasks);
-    }
-  };
-
-  const addTask = () => {
-    if (title.length < 1) {
-      return;
-    }
-
-    const newTask = {
-      // using nanoid to generate unique id
-      id: nanoid(),
-      title,
-    };
-    setTasks((prev) => prev.concat(newTask));
-    setTitle("");
-  };
-
-  const handleSearch = (ev: ChangeEvent<HTMLInputElement>) => {
-    setSearchKeyword(ev.target.value);
-  };
-
-  const filteredTasks = tasks.filter((task) =>
-    task.title.toLowerCase().includes(searchKeyword.toLowerCase()),
-  );
+  const {completeTask, updateTask, handleSearch, addTask, filteredTasks, title, setTitle} = useTaskManager();
 
   return (
     <div className="container">
